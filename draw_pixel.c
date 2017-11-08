9151177906e43eb99389605e4858dc94480e99a3
Jakub Hladik <jhladik@iastate.edu>
2017-11-07 22:59:31 -0600

    Drawing one pixel using xlib

---
+++ draw_pixel.c
@@ -0,0 +1,38 @@
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char **argv)
+{
+	Display *display = XOpenDisplay(NULL);
+	int screen_num = DefaultScreen(display);
+	Window root = RootWindow(display, screen_num);
+
+	Window win = XCreateSimpleWindow(display, root, 0, 0, 640, 480, 2,
+		WhitePixel(display, screen_num),BlackPixel(display, screen_num));
+
+	XMapWindow(display, win);
+	XSelectInput(display, win, ExposureMask | KeyPressMask);
+	GC gc = XCreateGC(display, win, 0, NULL);
+	XEvent event;
+	while(1)
+	{
+		XNextEvent(display, &event);
+		switch(event.type)
+		{
+			case Expose:
+				XSetForeground(display, gc, WhitePixel(display, screen_num));
+				XDrawPoint(display, win, gc, 10, 10);
+				break;
+			case KeyPress:
+				if(event.xkey.keycode == XKeysymToKeycode(display, XK_Escape))
+				{
+					exit(0);
+				}
+		}
+	}
+	XCloseDisplay(display);
+	return 0;
+}
+
